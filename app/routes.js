a88d066a8ef1e2b29e98cb3a24f1c5e48bcb5091
Carolina Junqueira <email>
2017-10-06 14:35:56 -0300

    organizacao pastas mvc

---
+++ app/routes.js
@@ -0,0 +1,23 @@
+module.exports = function(app) {
+
+    // blog home page
+    app.get('/', (req, res) => {
+        // render `index.ejs` with the list of posts
+        res.render('index')
+    })
+
+    app.get('/zones', (req, res) => {
+        // render `zones.ejs` with the list of posts
+        res.render('zones')
+    })
+
+    app.get('/comparing', (req, res) => {
+        // render `comparing.ejs` with the list of posts
+        res.render('comparing')
+    })
+
+    app.get('/statistics', (req, res) => {
+            // render `statistics.ejs` with the list of posts
+        res.render('statistics',{zoneSelected: 'Zone1', peak: 50, visitors: 80, customers: 20, totalPeople: 100, vendor: 'Apple', dayAverage: 35, days: ['24/10/17','25/10/17','26/10/17','27/10/17','28/10/17']}) 
+    })
+}
--- server.js
+++ server.js
@@ -1,33 +1,15 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
-const path = require('path');
 const app = express();
+
+// routes ==================================================
+require('./app/routes')(app); // configure our routes
+
+app.set('view engine', 'ejs');
 
 app.use(fileUpload());
 
 app.use('/public', express.static(__dirname + '/public'));
-app.set('view engine', 'ejs');
-
-// blog home page
-app.get('/', (req, res) => {
-  // render `index.ejs` with the list of posts
-  res.render('index')
-})
-
-app.get('/zones', (req, res) => {
-  // render `zones.ejs` with the list of posts
-  res.render('zones')
-})
-
-app.get('/comparing', (req, res) => {
-  // render `comparing.ejs` with the list of posts
-  res.render('comparing')
-})
-
-app.get('/statistics', (req, res) => {
-  // render `statistics.ejs` with the list of posts
-  res.render('statistics',{zoneSelected: 'Zone1', peak: 50, visitors: 80, customers: 20, totalPeople: 100, vendor: 'Apple', dayAverage: 35, days: ['24/10/17','25/10/17','26/10/17','27/10/17','28/10/17']}) 
-})
 
 app.post('/', function(req, res) {
   if (!req.files)
