786511a211d3268d8e8e753a7cb991243aaedc91
Carolina Junqueira <email>
2017-10-16 13:41:22 -0200

    correcao erros

--- app/models/scan.js
+++ app/models/scan.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 let scanSchema = new mongoose.Schema({
-  day: String,
-  hour: String,
-  zone: String,
-  macs: Array
+    day: String,
+    hour: String,
+    zone: String,
+    macs: Array
 }, { collection: 'scans' });
 
 module.exports = mongoose.model('ScanSchema',scanSchema);
--- app/routes.js
+++ app/routes.js
@@ -5,67 +5,79 @@ const getinfo = require('./models/getInfoZones')
 
 module.exports = function(app) {
 
-    // // blog home page
-    // app.get('/', (req, res) => {
-    //     // render `index.ejs` with the list of posts
-    //     res.render('zones')
-    // })
-
     app.get('/', (req, res) => {
         // render `zones.ejs` with the list of posts
         res.render('zones')
     })
 
     app.get('/comparing/:zone', function(req, res) {
-       let zone = req.params.zone;
-       Scans.find({'zone':zone}).lean().exec(
-          function (err, docs) {
-           if(!err){
-               let obj = getdata("",docs);
-               let arrayData = [];
-
-               for(let i = 0; i < obj.days.length; i++){
-                  let temp = getdata(obj.days[i].toString(),docs);
-                  arrayData.push(temp);
-                };
-                let arrayObj = getinfo(arrayData);
-                res.render('comparing',{obj: arrayObj});
-           }else{
-               console.log("Error! " + err.message);
-               return err;
-           }
-       });
-     });
+       	let zone = req.params.zone;
+       	Scans.find({'zone':zone}).lean().exec(
+          	function (err, docs) {
+				if(err){
+					res.render('error',{message:err.message,error: err});
+					return;
+				}
+				if(docs.length > 0){
+					let obj = getdata("",docs);
+					let arrayData = [];
+					
+					for(let i = 0; i < obj.days.length; i++){
+						let temp = getdata(obj.days[i].toString(),docs);
+						arrayData.push(temp);
+					};
+					let arrayObj = getinfo(arrayData);
+					res.render('comparing',{obj: arrayObj});
+				}else{
+					let err2 = {
+						status: '404 Not Found'
+					}
+					res.render('error',{message:"Sorry, Moongoose didn't find any document.",error: err2});
+				}
+      	});
+    });
 
     app.get('/statistics/:zone', function(req, res) {
-       let zone = req.params.zone;
-       Scans.find({'zone':zone}).lean().exec(
-          function (err, docs) {
-           if(!err){
-               let obj = getdata("",docs);
-               res.render('statistics', {obj: obj});
-           }else{
-               console.log("Error! " + err.message);
-               return err;
-           }
-       });
-     });
+      	let zone = req.params.zone;
+      	Scans.find({'zone':zone}).lean().exec(
+        	function (err, docs) {
+				if(err){
+					res.render('error',{message:err.message,error: err});
+					return;
+				}
+				if(docs.length > 0){
+					let obj = getdata("",docs);
+					res.render('statistics', {obj: obj});
+				}else{
+					let err2 = {
+						status: '404 Not Found'
+					}
+					res.render('error',{message:"Sorry, Moongoose didn't find any document.",error: err2});
+				}
+    		});
+    });
 
      app.get('/statistics/:zone/:year/:month/:day', function(req, res) {
-         let zone = req.params.zone;
-         let day = req.params.day;
-         let month = req.params.month;
-         let year = req.params.year;
-         let period = year+'-'+month+'-'+day;
-          Scans.find({'zone':zone}).lean().exec(
-             function (err, docs) {
-              if(!err){
-                  let obj = getdata(period,docs);
-                  res.render('statistics', {obj: obj})
-              }else{
-                  console.log("Error! " + err.message);
-                  return err;
-              }
-          });
-       });
+        let zone = req.params.zone;
+        let day = req.params.day;
+        let month = req.params.month;
+        let year = req.params.year;
+        let period = year+'-'+month+'-'+day;
+        Scans.find({'zone':zone}).lean().exec(
+            function (err, docs) {
+				if(err){
+					res.render('error',{message:err.message,error: err});
+					return;
+				}
+            	if(docs.length > 0){
+					let obj = getdata(period,docs);
+					res.render('statistics', {obj: obj})
+              	}else{
+					let err2 = {
+						status: '404 Not Found'
+					}
+					res.render('error',{message:"Sorry, Moongoose didn't find any document.",error: err2});
+				}
+          	});
+    });
 }
