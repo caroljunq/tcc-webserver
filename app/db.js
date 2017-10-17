57f7a5554345bdf1588aa12e65f0d728773019c9
Carolina Junqueira <email>
2017-10-17 14:02:51 -0200

    tratando o erro conexao banco

--- app/db.js
+++ app/db.js
@@ -1,2 +1,6 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://teste:teste123@ds113825.mlab.com:13825/gsmart");
+mongoose.connect("mongodb://teste:teste123@ds113825.mlab.com:13825/t",function(err){
+    if(err){
+        console.log(err);
+    }
+});
