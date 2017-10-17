50a7145f1a0418598efd6c1e8951078ff26f0eea
Carolina Junqueira <email>
2017-10-17 14:17:35 -0200

    bluebird promise mongo

--- app/db.js
+++ app/db.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const promise = require('bluebird');
+
+mongoose.Promise = promise;
 mongoose.connect("mongodb://teste:teste123@ds113825.mlab.com:13825/gsmart",{ useMongoClient: true },function(err){
     if(err)
         console.log(err);
