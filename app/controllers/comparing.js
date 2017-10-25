ac648362ec8c14534b48683d0de8624e95505a0c
Carol Junqueira <junqueiracarolina@live.com>
2017-10-24 22:34:18 -0200

    correcoes e comentarios

--- app/controllers/comparing.js
+++ app/controllers/comparing.js
@@ -1,3 +1,4 @@
+//Select zone
 function selectZone(sel){
   let option = sel.options[sel.selectedIndex];
   let form = document.querySelector('.select-zones-comparing');
@@ -5,13 +6,13 @@ function selectZone(sel){
   form.submit();
 }
 
-let drawBarChart = (data2) =>{
-  console.log(data2);
+//Draw Bar chart
+function drawBarChart(data){
   new Chart(document.getElementById("area-chart"), {
     type: 'bar',
     data: {
       labels: ['00:00','01:00','02:00','03:00','04:00','05:00','06:00','07:00','08:00','09:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00'],
-      datasets: data2
+      datasets: data
     },
     options: {
       title: {
