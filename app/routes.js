dcc18d5b2d0985c0837ed58b97ede17aba7b2afc
Carol Junqueira <junqueiracarolina@live.com>
2017-10-29 15:09:50 -0200

    teste curl

--- app/routes.js
+++ app/routes.js
@@ -86,16 +86,18 @@ module.exports = function(app) {
     });
 
     app.post('/upload', function(req, res) {
-        if (!req.files)
-            return res.status(400).send('No files were uploaded.');
+        console.log(req);
+        // console.log(req);
+        // if (!req.files)
+            // return res.status(400).send('No files were uploaded.');
 
         // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-        let sampleFile = req.files.sampleFile;
+        // let sampleFile = req.files.sampleFile;
 
         // Use the mv() method to place the file somewhere on your server
-        sampleFile.mv('./app/uploads/'+sampleFile.name, function(err) {
-            if (err)
-                return res.status(500).send(err);
+        // sampleFile.mv('./app/uploads/'+sampleFile.name, function(err) {
+            // if (err)
+                // return res.status(500).send(err);
         });
     });
 
