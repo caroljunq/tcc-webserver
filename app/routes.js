463eea34dfd854b0028b44b077a11607ab62c0bd
Carol Junqueira <junqueiracarolina@live.com>
2017-10-29 15:21:50 -0200

    correcao req. files

--- app/routes.js
+++ app/routes.js
@@ -87,7 +87,7 @@ module.exports = function(app) {
 
     app.post('/upload', function(req, res) {
 
-        if (!req.files)
+        if (!req.files.sampleFile)
             return res.status(400).send('No files were uploaded.');
 
         //The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
