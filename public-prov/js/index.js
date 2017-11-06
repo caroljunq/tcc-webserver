715cb3c60e7e51e04a541c420ff56accc75999ca
Carol Junqueira <junqueiracarolina@live.com>
2017-11-06 17:58:40 -0200

    bug select day

--- public-prov/js/index.js
+++ public-prov/js/index.js
@@ -1,10 +1,23 @@
-const inputFile = $('input[name=sampleFile]');
 
-inputFile.change(() => {
-    $("#textbox").html(inputFile.val());
-    $("#feedback").html("");
-});
+<!DOCTYPE html>
+<html>
+  <head>
+      <title>Lotus</title>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <style>
+      	body{
+      		background: url('download.jpg');
+      		width: 1000px;
+      		height: 1000px;
+      	}
+      </style>
 
-$("form").submit(() =>{
-    $("#feedback").html("File uploaded!");
-});
+  </head>
+  <body>
+      
+
+  </body>
+
+</html>
