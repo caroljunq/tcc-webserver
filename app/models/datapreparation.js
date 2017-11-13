85031078a42d7d220706fc254374529d3656cd34
Carol Junqueira <junqueiracarolina@live.com>
2017-11-12 23:38:29 -0200

    vendor primeira parte

--- app/models/datapreparation.js
+++ app/models/datapreparation.js
@@ -8,6 +8,7 @@ module.exports = prepareData = (fileName,fileText) => {
 
 		let register = fileName.split('_');
 
+		//delete duplicates in array
 		let macs = arr.filter((item, pos) => {
 		  	return arr.indexOf(item) == pos && item != ''; 
 		});
@@ -15,15 +16,17 @@ module.exports = prepareData = (fileName,fileText) => {
 		let total = macs.length;
 		let count = 0 ;
 		let processed = false;
+		let time = register[2].replace('-',':');
 		let scan = new Scans({
 			day: register[1],
-			hour: register[2],
+			hour: time,
 			zone: register[0],
 			macs: []
 		});
 
 		for(let i = 0; i < total; i++){
 			request("http://api.macvendors.com/"+macs[i], (error, response, body) => {
+				let seller = body.split(' ');
 				if(error){
 					processed  = true;
 					return rej(error);
@@ -31,11 +34,10 @@ module.exports = prepareData = (fileName,fileText) => {
   				++count;
   				scan.macs.push({
   					mac: macs[i],
-  					vendor: body
+  					vendor: seller[0]
   				});
 
   				if(!processed && count == total){
-
   					processed  = true;
   					scan.save((err) => {
   						if(err){
