9007d9f25f92b08d7e3f82130da9608d2f213e18
Carol Junqueira <junqueiracarolina@live.com>
2017-11-15 23:21:40 -0200

    adaptacoes customers read

--- app/models/datapreparation.js
+++ app/models/datapreparation.js
@@ -13,43 +13,71 @@ module.exports = prepareData = (fileName,fileText) => {
 		  	return arr.indexOf(item) == pos && item != ''; 
 		});
 
-		let total = macs.length;
-		let count = 0 ;
-		let processed = false;
-		let time = register[2].replace('-',':');
-		let customer = false;
+		let customers = [];
 
-		let scan = new Scans({
-			day: register[1],
-			hour: time,
-			zone: register[0],
-			macs: []
-		});
-
-		for(let i = 0; i < total; i++){
-			request("http://api.macvendors.com/"+macs[i], (error, response, body) => {
-				let seller = body.split(' ');
-				if(error){
-					processed  = true;
-					return rej(error);
+		let aux = [];
+		Scans.find({'zone': register[0]},function(err, doc){
+			
+			for(let k = 0 ; k < doc.length; k++){
+				for(let j = 0; j < doc[k].macs.length; j++){
+						aux.push(doc[k].macs[j].mac);
 				}
-  				++count;
-  				scan.macs.push({
-  					mac: macs[i],
-  					vendor: seller[0]
-  				});
+			}
 
-  				if(!processed && count == total){
-  					processed  = true;
-  					scan.save((err) => {
-  						if(err){
-  							rej(err);
-  						}
-  						res(scan);
-  					});
-  				}
+			let zoneMacs = aux.filter((item, pos) => {
+		  		return aux.indexOf(item) == pos; 
+			});	
+				
+			for(let k = 0; k < macs.length; k++){
+
+				if(zoneMacs.indexOf(macs[k]) == -1){
+					customers[k] = false;	
+				}else{ 
+				
+					customers[k]= true;
+				}
+			}
+
+			let total = macs.length;
+			let count = 0 ;
+			let processed = false;
+			let time = register[2].replace('-',':');
+			let customer = false;
+
+			let scan = new Scans({
+				day: register[1],
+				hour: time,
+				zone: register[0],
+				macs: []
 			});
-		}
+
+			for(let i = 0; i < total; i++){
+				request("http://api.macvendors.com/"+macs[i], (error, response, body) => {
+					let seller = body;
+					let vendor = seller.split(' ');
+					if(error){
+						processed  = true;
+						return rej(error);
+					}
+	  				++count;
+	  				scan.macs.push({
+	  					mac: macs[i],
+	  					vendor: vendor[0],
+	  					customer: customers[i]
+	  				});
+
+	  				if(!processed && count == total){
+	  					processed  = true;
+	  					scan.save((err) => {
+	  						if(err){
+	  							rej(err);
+	  						}
+	  						res(scan);
+	  					});
+	  				}
+				});
+			}
+		});
 	});
 
 }
